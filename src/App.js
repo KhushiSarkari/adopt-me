596670151a10abf250ce5a6a8541639a45c1c4ca
Khushi Sarkari <karkari@ex2india.com>
2020-09-02 14:02:52 +0530

    add npm scripts, eslint, Prettier, gitignore

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@ const Pet = ({ name, animal, breed }) => {
     return React.createElement("div", {}, [
         React.createElement("h1", {}, name),
         React.createElement("h2", {}, animal),
-        React.createElement("h2", {}, breed)
+        React.createElement("h2", {}, breed),
     ]);
-
-}
+};
 
 const App = () => {
     return React.createElement("div", {}, [
@@ -13,19 +12,18 @@ const App = () => {
         React.createElement(Pet, {
             name: "Luna",
             animal: "Dog",
-            breed: "Havanese"
+            breed: "Havanese",
         }),
         React.createElement(Pet, {
             name: "Pepper",
             animal: "Bird",
-            breed: "Cockatiel"
+            breed: "Cockatiel",
         }),
         React.createElement(Pet, {
             name: "Doink",
             animal: "Cat",
-            breed: "Mixed"
+            breed: "Mixed",
         }),
     ]);
-
-}
+};
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
