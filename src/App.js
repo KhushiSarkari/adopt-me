aba39afb44d2740c2d416389f63828b736d24b52
Khushi Sarkari <karkari@ex2india.com>
2020-09-02 17:29:52 +0530

    convert to jsx

--- src/App.js
+++ src/App.js
@@ -3,23 +3,15 @@ import ReactDOM from 'react-dom';
 import Pet from './Pet';
 
 const App = () => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, "Adopt Me!"),
-        React.createElement(Pet, {
-            name: "Luna",
-            animal: "Dog",
-            breed: "Havanese"
-        }),
-        React.createElement(Pet, {
-            name: "Pepper",
-            animal: "Bird",
-            breed: "Cockatiel"
-        }),
-        React.createElement(Pet, {
-            name: "Doink",
-            animal: "Cat",
-            breed: "Mixed"
-        }),
-    ]);
-};
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+    return (
+        <div>
+            <h1>Adopt Me!</h1>
+            <Pet name="Luna" animal="Dog" breed="Havanese" />
+            <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+            <Pet name="Doink" animal="Cat" breed="Mixed" />
+        </div>
+    )
+
+
+}
+ReactDOM.render(<App />, document.getElementById("root"));
