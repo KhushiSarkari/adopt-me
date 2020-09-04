c6755cf1291218604d1a3b1f0a3ef547ccc15716
Khushi Sarkari <ksarkari@ex2india.com>
2020-09-04 15:33:03 +0530

    request pets on form submission

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import pet, { ANIMALS } from '@frontendmasters/pet';
+import Results from './Results';
 import useDropdown from './useDropdown';
 
 const SearchParams = () => {
@@ -7,7 +8,16 @@ const SearchParams = () => {
     const [breeds, setBreeds] = useState([]);
     const [animal, AnimalDropdown] = useDropdown("animal", "dog", ANIMALS);
     const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+    const [pets, setPets] = useState([]);
 
+    async function requestPets() {
+        const { animals } = await pet.animals({
+            location,
+            breed,
+            type: animal
+        });
+        setPets(animals || []);
+    }
     useEffect(() => {
         setBreeds([]);
         setBreed("");
@@ -20,7 +30,10 @@ const SearchParams = () => {
 
     return (
         <div className="search-params">
-            <form>
+            <form onSubmit={e => {
+                e.preventDefault();
+                requestPets();
+            }}>
                 <label htmlFor="location">
                     Location
                     <input
@@ -33,6 +46,7 @@ const SearchParams = () => {
                 <BreedDropdown />
                 <button>Submit</button>
             </form>
+            <Results pets={pets} />
         </div>
     )
 }
