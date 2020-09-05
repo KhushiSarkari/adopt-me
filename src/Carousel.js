b1616ba06e0b8c3c81d354b3b67ff7fe2d3b7731
Khushi Sarkari <ksarkari@ex2india.com>
2020-09-05 23:57:20 +0530

    Carousel implementation

---
+++ src/Carousel.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+class Carousel extends React.Component {
+    state = {
+        photos: [],
+        active: 0
+    };
+    // constructor(props) {
+    //     super(props);
+    //     //alternative to bind the context of handleIndexclick to the Carousel 
+    //     this.handleIndexClick = this.handleIndexClick.bind(this);
+
+    // }
+    static getDerivedStateFromProps({ media }) {
+        let photos = ["http://placecorgi.com/600/600"];
+
+        if (media.length) {
+            photos = media.map(({ large }) => large);
+        }
+
+        return { photos };
+    }
+    handleIndexClick = (e) => {
+        this.setState({
+            active: +e.target.dataset.index
+        });
+    }
+    render() {
+        const { photos, active } = this.state;
+        return (
+            <div className="carousel">
+                <img src={photos[active]} alt="animal" />
+                <div className="carousel-smaller">
+                    {photos.map((photo, idx) => (
+                        //eslint-disable-next-line
+                        <img
+                            key={photo}
+                            onClick={this.handleIndexClick}
+                            data-index={idx}
+                            src={photo}
+                            className={idx === active ? "active" : ""}
+                            alt="animal-thumbnail"
+                        />
+                    ))}
+                </div>
+            </div >
+        )
+
+    }
+}
+export default Carousel;
